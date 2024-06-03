7132053ef11083eaff615ea981b5fe64098a2515
Vaibhav Kulkarni <vaibhav.kulkarni0359@gmail.com>
2024-06-03 18:51:23 +0530

    Splice()

--- Basic Methods/Storing in Local Variables/fucntion.js
+++ Basic Methods/Storing in Local Variables/fucntion.js
@@ -1,4 +1,4 @@
-
+/*
 let buttonElement = document.getElementById("saveButton");
 let textConatiner = document.getElementById("textArea");
 
@@ -17,3 +17,41 @@ if (textvalue === null){
 else{
     textConatiner.value = storedValue; 
 }
+
+*/
+
+// Splice 
+
+let Myarray = [5,"six",2,5.8];
+let deletedItem = Myarray.splice(2,2);
+console.log(deletedItem);
+
+// Replacing items using splice 
+
+Myarray.splice(2,2,"one","Flase");
+console.log(Myarray); 
+
+// adding
+
+Myarray.splice(2,0,1);
+
+// knowing the index of an item
+
+let array = [5,12,3,5,7,8];
+let indexItem = array.findIndex(function(eachitem){
+    if (eachitem === 8){
+        return true;
+    }
+    else{
+        return false;
+    }
+});
+console.log(indexItem);
+
+
+
+
+
+
+
+
